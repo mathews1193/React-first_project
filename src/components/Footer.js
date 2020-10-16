eb014bf72c72a03d9bd69efbda5c7bcda9083f60
mathews1193 <mathews1193@yahoo.com>
2020-10-15 22:42:09 -0500

    function footer changed to class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './stylesheets/Footer.css';
 
-function Footer() {
-    const name = "Sous Chef";
+class Footer extends React.Component {
+    
+    render(){
+        const name = "Sous Chef";
     const owner = "Donald Mathews";
 
     return(
         <footer className="bottom">
             <p className='message'>@2020 {name} by Owner: {owner}</p>
         </footer>
-    )
+        )
+    }
+    
 }
 
 export default Footer
