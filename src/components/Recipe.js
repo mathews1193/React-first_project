80f99499b8306ad01bc8a37afae396deed0b7802
mathews1193 <mathews1193@yahoo.com>
2020-10-15 23:19:47 -0500

    change function recipe into class

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,22 +1,24 @@
 import React from "react";
 import './stylesheets/Recipes.css';
 
-function Recipe(props) {
-    return(
+class Recipe extends React.Component {
+
+    render(){
+        return(
         <div className="recipe">
-            <img src={props.recipe.imgUrl} alt="Food" width="200" height="200"/>
-            <h2>{props.recipe.food}</h2>
-            <h3>Category: {props.recipe.category}</h3>
+            <img src={this.props.recipe.imgUrl} alt="Food" width="200" height="200"/>
+            <h2>{this.props.recipe.food}</h2>
+            <h3>Category: {this.props.recipe.category}</h3>
             <h4>Ingredients</h4>
             <ul>
-                <li>{props.recipe.item1}</li>
-                <li>{props.recipe.item2}</li>
-                <li>{props.recipe.item3}</li>
-                <li>{props.recipe.item4}</li>
+                <li>{this.props.recipe.item1}</li>
+                <li>{this.props.recipe.item2}</li>
+                <li>{this.props.recipe.item3}</li>
+                <li>{this.props.recipe.item4}</li>
             </ul>
 
         </div>
-    )
-    
+        )
+    } 
 }
 export default Recipe
