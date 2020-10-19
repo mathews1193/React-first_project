d3b5fb7d193243f613d3f04a2707592d7b5c4ff8
mathews1193 <mathews1193@yahoo.com>
2020-10-19 16:58:31 -0500

    updated dropdown navbar and sign up button

--- src/components/DropDownNav.js
+++ src/components/DropDownNav.js
@@ -5,10 +5,12 @@ class DropDownNav extends React.Component {
     render(){
          return (
             <div class="dropdown">
-              <button class="dropbtn">Power</button>
+              <button class="dropbtn">
+                <img src="power.png" alt="power" width="50" height="50"></img>
+              </button>
               <div class="dropdown-content">
-                  <a>Profile</a>
-                  <a>Setting</a>
+                  <a href="#">Profile</a>
+                  <a href="#">Setting</a>
                   <a href="#">Log Out</a>
               </div>
           </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,23 @@ import React from 'react';
 import DropDownNav from './DropDownNav';
 import './stylesheets/NavBar.css';
 
+function signIn(){
+    console.log("Signed in")
+}
+
 class NavBar extends React.Component{
     
     render(){
         return (
         <nav className='NavBar'>
             <ul className='links'>
-                <li className="nav">Sous Chef</li>
+                <li className="logo">Sous Chef</li>
                 <li className="nav">Home</li>
                 <li className="nav">About Us</li>
                 <li className="nav">Orders</li>
                 <li className="nav">Recipes</li>
                 <li className="nav">Contact</li>
-                <li className="btn-signup">Sign Up</li>
+                <button className="btn-signup" onClick={signIn}>Sign Up</button>
                 <DropDownNav/>
             </ul>
         </nav>
