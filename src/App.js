4c22dfeac138bda450f3b5bc23a9d1945d8c72d2
mathews1193 <mathews1193@yahoo.com>
2020-10-26 09:05:52 -0500

    created a recipe list to maintain database

--- src/App.js
+++ src/App.js
@@ -3,33 +3,23 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import './App.css';
-import Recipe from './components/Recipe';
-import RecipeData from './components/RecipeData';
+import RecipeList from './components/RecipeList';
 import MyInfor from './components/MyInfor';
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      recipe:RecipeData
-    }
-  }
   render(){
-     const recipeComponents = this.state.recipe.map(recipes =>
-    <Recipe recipe={recipes}/>)
 
   return (
     <div>
       <div className="container">
         <NavBar/>
-         {/* */}
             <div className="container-todo">
               <MyInfor/> 
             </div>
-          <div className="recipe-list">
-            {recipeComponents} 
-          </div>
+              <div className="recipe-list">
+                    <RecipeList />
+              </div>
       </div>
         <Footer/>
     </div>
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,15 +1,29 @@
 
 import React from 'react';
+import Recipe from './Recipe';
+import RecipeData from './RecipeData';
 
-class Signup extends React.Component {
+class RecipeList extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+          recipe:RecipeData
+        }
+      }
+    
     render() {
+        const recipeComponents = this.state.recipe.map(recipes =>
+        <Recipe recipe={recipes}/>)
+
         return (
             <div>
-                <h1>Sign up</h1>
+                <div className="recipe-list">
+                    {recipeComponents} 
+                </div>
             </div>
         )
     }
 }
 
-export default Signup
+export default RecipeList
